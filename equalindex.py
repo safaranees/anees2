05968ae8c45e3f4b697e7bb4f596d4b6a856bc15
safaranees <aneesparvin08@gmail.com>
2019-06-14 16:18:14 +0530

    Update equalindex.py

--- equalindex.py
+++ equalindex.py
@@ -1,5 +1,11 @@
-num1=int(input())
-a1=list(map(int,input().split()))
-for i in range(0,num1):
-  if(i==a1[i]):
-    print(i)
+nn=int(input())
+ll=[]
+array=list(map(int,input().split()))
+for x in range(0,nn):
+    if(array[x]==x):
+        ll.append(x)
+if(len(ll)==0):
+    print("-1")
+ll.sort()
+for x in ll:
+    print(x,end=" ")
