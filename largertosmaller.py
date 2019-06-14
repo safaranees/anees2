3373bf2e9fb8403ba652ee8978059f0b46ec4b5e
safaranees <aneesparvin08@gmail.com>
2019-06-14 16:20:56 +0530

    Update largertosmaller.py

--- largertosmaller.py
+++ largertosmaller.py
@@ -1,5 +1,10 @@
-num=int(input())
-num2=input()
-num2=num2.split()
-num2.sort(reverse=True)
-print(*num2,sep="")
+xx1=int(input())
+ss1=list(map(int,input().split()))
+yy1=sorted(ss1)[::-1]
+zz1=""
+if(ss1==[0]*xx1):
+    print("0")
+else:
+    for j in yy1:
+        zz1=zz1+str(j)
+    print(int(zz1))
